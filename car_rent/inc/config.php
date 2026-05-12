508319491ef397cc39153fe7175d963ad370653a
Yuna <yuna@hkhk.edu.ee>
2026-05-12 15:09:49 +0300

    Remove XAMPP configuration comment from config.php
    Removed comment regarding XAMPP configuration.

--- car_rent/inc/config.php
+++ car_rent/inc/config.php
@@ -3,7 +3,7 @@ if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
 
-// Muuda ainult neid andmeid, kui sinu XAMPP seadistus on teine.
+
 define('DB_HOST', '127.0.0.1');
 define('DB_NAME', 'autorent');
 define('DB_USER', 'root');
